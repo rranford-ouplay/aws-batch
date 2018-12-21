8eb6ed57ef8983abccb9723c655f44210f5e981a
Robyn Ranford <robyn.ranford@outplay.com>
2018-12-21 10:15:16 +0000

    use custom dynamodb table name

--- job.sh
+++ job.sh
@@ -29,7 +29,7 @@ rm item.json
 echo $Fib
 echo "{\"jobID\": {\"S\": \"$AWS_BATCH_JOB_ID\"}, \"Fibi\": {\"S\": \"$Fib\"}}" > item.json
 cat item.json
-RESP=$(aws dynamodb put-item --region us-east-1 --table-name fetch_and_run --item file://item.json)
+RESP=$(aws dynamodb put-item --region us-east-1 --table-name rar_fetch_and_run --item file://item.json)
 echo $RESP
 
 sleep 1
